5b0864a19d9e4c940c89eed58da62b1042bfde61
Serena <serenafgreen@gmail.com>
2024-03-17 23:44:25 +0000

    Changes background image and about description

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,35 +7,36 @@
 import React from "react";
 
 /**
+ * 
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/beach-jeep.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "sunset background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a software engineering student studying at Actualize. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "Programming",
+  "UI design",
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Debugging",
   "Graphic design",
 ];
 
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive performance. By leveraging my programming and UI experience, I continually look for new and better ways to make tech accessible and easy to use.";
 
 const About = () => {
   return (
